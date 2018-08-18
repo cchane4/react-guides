4e86c1e54a44b5214027406c1b46cd75e683bb86
Chad Chaney <cchane4@gmail.com>
2018-08-18 18:46:40 -0500

    showing how the compiler works

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*this react import statement is needed to rendering to the DOM*/
 
+import React, { Component } from 'react';
+import './App.css';
+import Person from "./Person/Person"; 
+// react calls render method to render something to the screen 
+// IMPORTANT :every react component has to return or render some html code to the DOM
 class App extends Component {
   render() {
     return (
+      // this code is actually javascript 
+      // its just syntactical sugar 
+      //everything is nested inside one root element (div with App class)
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1> Hi, Im a React App</h1>
+      <p> This is really working</p>
+      <Person></Person>
       </div>
+
     );
+    // create a div with a javascript object of class named "App"; that object has nested 
+    // inside of it, create an h1 element with a null value but attach to the h1 element some text "Hi Im a React App"
+   // return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Hi, Im a React App')); 
   }
 }
-
+// if you import the whole file, you  by default import the App class.
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+// a component at its core is just a function that returns html 
+import React from 'react'; 
+
+const person = () => { 
+    return <p> I'm a person!</p>
+
+}; 
+
+export default person; 
